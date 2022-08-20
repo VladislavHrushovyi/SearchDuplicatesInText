a1a3ea043eef8f923c2027ef6371beaf42bbfec1
Vlad Hrushovyi <vlad.grush.99@gmail.com>
2022-08-20 12:25:56 +0300

    add watch

--- SearchDuplicatesText/Services/FileService/UploadFileService.cs
+++ SearchDuplicatesText/Services/FileService/UploadFileService.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using SearchDuplicatesText.Services.MakeDataForMethodsService;
 
 namespace SearchDuplicatesText.Services.FileService;
@@ -15,13 +16,17 @@ public class UploadFileService
 
     public async Task<IResult> SaveUploadFile(IFormFile file)
     {
+        var watch = new Stopwatch();
+        watch.Start();
         var fileReader = await _fileHandler.GetFileReader(file);
         var filterTextResult = await _fileHandler.GetNewFileData(fileReader);
         var fileName = await _fileHandler.SaveFile(fileReader, filterTextResult);
 
         await _convertText.CreateShingleFile(filterTextResult, fileName);
         await _convertText.CreateNgramFile(filterTextResult, fileName);
-
+        
+        watch.Stop();
+        Console.WriteLine($"Add {fileName} on {watch.Elapsed}");
         return Results.Ok(new
         {
             name = fileName
